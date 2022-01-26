744b06f761280dc138b389b626379946a9abdb8a
Jason Henderson <jason.henderson3@mail.dcu.ie>
2022-01-26 20:44:23 +0000

    server/models: update user file

--- src/server/models/user.go
+++ src/server/models/user.go
@@ -1,8 +1,32 @@
 package models
 
+import (
+	"fmt"
+	"net/http"
+)
+
 type User struct {
 	ID            int    `json:"id"`
 	Name          string `json:"name"`
 	PhoneNumber   string `json:"phone_number"`
 	CovidPositive bool   `json:"covid_positive"`
+}
+
+type UserList struct {
+	Users []User `json:"users"`
+}
+
+func (u *User) Bind(r *http.Request) error {
+	if u.Name == "" {
+		return fmt.Errorf("name is a required field")
+	}
+	return nil
+}
+
+func (*UserList) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (*User) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
 }
