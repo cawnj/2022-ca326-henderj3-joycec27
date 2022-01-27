095d42a00d1c8214af788ea1ec2759c7ec4aee7b
Conor Joyce <conor.joyce27@mail.dcu.ie>
2022-01-27 13:48:28 +0000

    server: add user POST endpoint

--- src/server/main.go
+++ src/server/main.go
@@ -6,14 +6,26 @@ import (
 	"net/http"
 
 	"sonic-server/db"
+	"sonic-server/models"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 )
 
 func main() {
-	router := chi.NewRouter()
-	router.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Alive!")
+	})
+	r.Post("/user", func(w http.ResponseWriter, r *http.Request) {
+		data := &models.User{}
+		if err := render.Bind(r, data); err != nil {
+			fmt.Fprint(w, "Error")
+			return
+		}
+		render.JSON(w, r, data)
 	})
 
 	log.Println("Attemping connection to database")
@@ -23,7 +35,7 @@ func main() {
 	}
 
 	log.Println("Server starting at http://localhost:8080")
-	err = http.ListenAndServe(":8080", router)
+	err = http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatalf("Error starting server: %s", err)
 	}
