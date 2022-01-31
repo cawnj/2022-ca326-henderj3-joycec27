7cd980a0deb4d7e5cc1782b061556442fabd6417
Jason Henderson <jason.henderson3@mail.dcu.ie>
2022-01-31 19:30:05 +0000

    server/models: add bindings to location.go

--- src/server/models/location.go
+++ src/server/models/location.go
@@ -1,9 +1,33 @@
 package models
 
-import geo "github.com/kellydunn/golang-geo"
+import (
+	"fmt"
+	"net/http"
+
+	geo "github.com/kellydunn/golang-geo"
+)
 
 type Location struct {
 	ID     int       `json:"id"`
 	Name   string    `json:"name"`
 	Coords geo.Point `json:"coords"`
+}
+
+type LocationList struct {
+	Locations []Location `json:"locations"`
+}
+
+func (u *Location) Bind(r *http.Request) error {
+	if u.Name == "" {
+		return fmt.Errorf("name is a required field")
+	}
+	return nil
+}
+
+func (*LocationList) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (*Location) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
 }
