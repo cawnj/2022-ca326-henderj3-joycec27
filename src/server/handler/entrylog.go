7a8bc5c9bf1a10b44d1e2f019232b4c9875007e7
Conor Joyce <conor.joyce27@mail.dcu.ie>
2022-02-03 22:21:18 +0000

    server/handler: update entrylog endpoint to respond with json

--- src/server/handler/entrylog.go
+++ src/server/handler/entrylog.go
@@ -12,6 +12,13 @@ type EntryLog struct {
 	Data string `json:"data"`
 }
 
+func respondwithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
 func entrylog(router chi.Router) {
 	router.Post("/", writeEntryLog)
 }
@@ -20,4 +27,6 @@ func writeEntryLog(w http.ResponseWriter, r *http.Request) {
 	var entryLog EntryLog
 	json.NewDecoder(r.Body).Decode(&entryLog)
 	log.Printf("Received: %s", entryLog.Data)
+
+	respondwithJSON(w, http.StatusCreated, map[string]string{"message": "log successful"})
 }
