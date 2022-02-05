ad61791d572dea33aa02412357309c8392feb3f8
Conor Joyce <conor.joyce27@mail.dcu.ie>
2022-02-05 19:41:00 +0000

    server/handler: modify entrylog to insert request data into db

--- src/server/handler/entrylog.go
+++ src/server/handler/entrylog.go
@@ -1,32 +1,30 @@
 package handler
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
 
+	"sonic-server/models"
+
 	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
 )
-
-type EntryLog struct {
-	Data string `json:"data"`
-}
-
-func respondwithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(response)
-}
 
 func entrylog(router chi.Router) {
 	router.Post("/", writeEntryLog)
 }
 
 func writeEntryLog(w http.ResponseWriter, r *http.Request) {
-	var entryLog EntryLog
-	json.NewDecoder(r.Body).Decode(&entryLog)
-	log.Printf("Received: %s", entryLog.Data)
-
-	respondwithJSON(w, http.StatusCreated, map[string]string{"message": "log successful"})
+	entryLog := &models.EntryLog{}
+	if err := render.Bind(r, entryLog); err != nil {
+		render.Render(w, r, ErrBadRequest)
+		return
+	}
+	if err := dbInstance.AddEntryLog(entryLog); err != nil {
+		render.Render(w, r, ErrorRenderer(err))
+		return
+	}
+	if err := render.Render(w, r, entryLog); err != nil {
+		render.Render(w, r, ServerErrorRenderer(err))
+		return
+	}
 }
