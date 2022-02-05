8570e65fed38cffed82f5de5344e93b36cebdab0
Conor Joyce <conor.joyce27@mail.dcu.ie>
2022-02-05 20:08:32 +0000

    client/scanner: update to send valid entrylog data to server

--- src/client/scanner.py
+++ src/client/scanner.py
@@ -4,8 +4,11 @@ import nfc
 import ndef
 import requests
 from nfc.clf import RemoteTarget
-from time import sleep
+from time import sleep, gmtime, strftime
 
+
+LOCATION_ID = 1
+API_ENDPOINT = "https://sonic.cawnj.dev/entrylog"
 
 def find_tag(clf):
     print("\nWaiting for target...")
@@ -27,15 +30,30 @@ def read_tag(tag):
         return None
     return records[0].text
 
+def get_user_id(tag):
+    return int(read_tag(tag))
+
+def send_post_request(user_id):
+    data = {
+        "user_id": user_id,
+        "location_id": LOCATION_ID,
+        "entry_time": strftime("%Y-%m-%d %H:%M:%S", gmtime()),
+        "exit_time": "3000-01-01 00:00:00"
+    }
+    print(data)
+    response = requests.post(url=API_ENDPOINT, json=data)
+    try:
+        print(response.json())
+    except:
+        print("Something went wrong sending POST request")
+
 def main():
     with nfc.ContactlessFrontend('tty:S0:pn532') as clf:
         while True:
             tag = find_tag(clf)
             if tag.ndef:
-                data = read_tag(tag)
-                url = "https://sonic.cawnj.dev/entrylog"
-                response = requests.post(url, json={"data": data})
-                print(response.json())
+                user_id = get_user_id(tag)
+                send_post_request(user_id)
                 break
 
 if __name__ == "__main__":
