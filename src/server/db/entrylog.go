db4e318817ffe4ee1af3ab3019224e0f667daf1e
Jason Henderson <jason.henderson3@mail.dcu.ie>
2022-02-07 20:35:44 +0000

    server/db: remove entrylog list

--- src/server/db/entrylog.go
+++ src/server/db/entrylog.go
@@ -4,23 +4,6 @@ import (
 	"sonic-server/models"
 )
 
-func (db Database) GetAllEntryLogs() (*models.EntryLogList, error) {
-	entryLogs := &models.EntryLogList{}
-	rows, err := db.Conn.Query("SELECT * FROM entry_log ORDER BY entry_id DESC")
-	if err != nil {
-		return entryLogs, err
-	}
-	for rows.Next() {
-		var entryLog models.EntryLog
-		err := rows.Scan(&entryLog.UserID, &entryLog.LocationID, &entryLog.EntryTime, &entryLog.ExitTime)
-		if err != nil {
-			return entryLogs, err
-		}
-		entryLogs.EntryLogs = append(entryLogs.EntryLogs, entryLog)
-	}
-	return entryLogs, nil
-}
-
 func (db Database) AddEntryLog(entryLog *models.EntryLog) error {
 	var id int
 	query := `INSERT INTO entry_log (user_id, location_id, entry_time)
