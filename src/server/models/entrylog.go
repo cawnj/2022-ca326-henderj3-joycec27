52b51868b6398742eb0533adc3bf7390b34771cf
Jason Henderson <jason.henderson3@mail.dcu.ie>
2022-02-07 20:32:09 +0000

    server/models: refactor entrylog models

--- src/server/models/entrylog.go
+++ src/server/models/entrylog.go
@@ -7,14 +7,6 @@ import (
 )
 
 type EntryLog struct {
-	EntryID    int    `json:"entry_id"`
-	UserID     int    `json:"user_id"`
-	LocationID int    `json:"location_id"`
-	EntryTime  string `json:"entry_time"`
-	ExitTime   string `json:"exit_time"`
-}
-
-type DBEntryLog struct {
 	EntryID    int            `json:"entry_id"`
 	UserID     int            `json:"user_id"`
 	LocationID int            `json:"location_id"`
@@ -22,11 +14,10 @@ type DBEntryLog struct {
 	ExitTime   sql.NullString `json:"exit_time"`
 }
 
-type EntryLogList struct {
-	EntryLogs []EntryLog `json:"entry_logs"`
-}
-
 func (u *EntryLog) Bind(r *http.Request) error {
+	if u.EntryID == 0 {
+		return fmt.Errorf("entry_id is a required field")
+	}
 	if u.UserID == 0 {
 		return fmt.Errorf("user_id is a required field")
 	}
@@ -36,14 +27,6 @@ func (u *EntryLog) Bind(r *http.Request) error {
 	return nil
 }
 
-func (*EntryLogList) Render(w http.ResponseWriter, r *http.Request) error {
-	return nil
-}
-
 func (*EntryLog) Render(w http.ResponseWriter, r *http.Request) error {
-	return nil
-}
-
-func (*DBEntryLog) Render(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
---
+++ src/server/models/entrylogrequest.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type EntryLogRequest struct {
+	UserID     int    `json:"user_id"`
+	LocationID int    `json:"location_id"`
+	Timestamp  string `json:"timestamp"`
+}
+
+func (u *EntryLogRequest) Bind(r *http.Request) error {
+	if u.UserID == 0 {
+		return fmt.Errorf("user_id is a required field")
+	}
+	if u.LocationID == 0 {
+		return fmt.Errorf("location_id is a required field")
+	}
+	if u.Timestamp == "" {
+		return fmt.Errorf("timestamp is a required field")
+	}
+	return nil
+}
+
+func (*EntryLogRequest) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
