3ca866fc104c1015aedc232c9044ad00153c305d
Conor Joyce <conor.joyce27@mail.dcu.ie>
2022-02-08 16:02:43 +0000

    server/db: add more test data to migrations

--- src/server/db/migrations/000002_add_test_data.up.sql
+++ src/server/db/migrations/000002_add_test_data.up.sql
@@ -3,21 +3,51 @@
 INSERT INTO users (name, phone_number, covid_positive)
 VALUES
 (
-    'John Doe',
+    'Conor Joyce',
     '0851234123',
+    FALSE
+),
+(
+    'Jason Henderson',
+    '0854567456',
+    FALSE
+),
+(
+    'John Appleseed',
+    '0857890789',
     FALSE
 );
 INSERT INTO locations (name, coords)
 VALUES
 (
     'The Spire',
-    ST_SetSRID(ST_MakePoint(53.3498133103617, -6.260254521788297), 4326)
+    ST_SetSRID(ST_MakePoint(53.349813, -6.260254), 4326)
+),
+(
+    'DCU Nubar',
+    ST_SetSRID(ST_MakePoint(53.385006, -6.258963), 4326)
+),
+(
+    'The Academy',
+    ST_SetSRID(ST_MakePoint(53.348001, -6.261989), 4326)
 );
 INSERT INTO entry_log (user_id, location_id, entry_time, exit_time)
 VALUES
 (
     1,
-    1,
+    3,
     DATE_TRUNC('second', NOW()::timestamp),
     DATE_TRUNC('second', (NOW() + interval '1 hour')::timestamp)
+),
+(
+    2,
+    2,
+    DATE_TRUNC('second', NOW()::timestamp),
+    DATE_TRUNC('second', (NOW() + interval '1 hour')::timestamp)
+),
+(
+    3,
+    2,
+    DATE_TRUNC('second', (NOW() - interval '1 hour')::timestamp),
+    DATE_TRUNC('second', (NOW() + interval '30 minutes')::timestamp)
 );
