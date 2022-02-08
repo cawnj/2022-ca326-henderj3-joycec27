df6e90a66fa41be76669924d12d56b61452e3af9
Conor Joyce <conor.joyce27@mail.dcu.ie>
2022-02-08 16:45:43 +0000

    server/db: add logic for contact tracing events

--- src/server/db/entrylog.go
+++ src/server/db/entrylog.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"log"
 	"sonic-server/models"
 )
 
@@ -76,24 +77,61 @@ func (db Database) GetContactUsers(userId int) (*models.EntryLogList, error) {
 	entryLogs := &models.EntryLogList{}
 	query := `SELECT * FROM entry_log
 	WHERE user_id = $1`
-	rows, err := db.Conn.Query(query, userId)
+	contactEvents, err := db.Conn.Query(query, userId)
 	if err != nil {
 		return entryLogs, err
 	}
 
-	for rows.Next() {
-		var entryLog models.EntryLog
-		err := rows.Scan(
-			&entryLog.EntryID,
-			&entryLog.UserID,
-			&entryLog.LocationID,
-			&entryLog.EntryTime,
-			&entryLog.ExitTime,
+	for contactEvents.Next() {
+		var contactEvent models.EntryLog
+		err := contactEvents.Scan(
+			&contactEvent.EntryID,
+			&contactEvent.UserID,
+			&contactEvent.LocationID,
+			&contactEvent.EntryTime,
+			&contactEvent.ExitTime,
 		)
 		if err != nil {
 			return entryLogs, err
 		}
-		entryLogs.EntryLogs = append(entryLogs.EntryLogs, entryLog)
+
+		log.Printf("%+v", contactEvent)
+		query = `SELECT * FROM entry_log
+		WHERE user_id != $1
+		AND location_id = $2
+		AND (
+			(entry_time <= $3 AND exit_time <= $4 AND exit_time >= $3)
+			OR
+			(entry_time <= $3 AND exit_time >= $4)
+			OR
+			(entry_time >= $3 AND exit_time <= $4)
+			OR
+			(entry_time >= $3 AND exit_time >= $4 AND entry_time <= $4)
+		)`
+		contactUserLogs, err := db.Conn.Query(
+			query,
+			userId,
+			contactEvent.LocationID,
+			contactEvent.EntryTime.String,
+			contactEvent.ExitTime.String,
+		)
+		if err != nil {
+			return entryLogs, err
+		}
+		for contactUserLogs.Next() {
+			var contactUserLog models.EntryLog
+			err := contactUserLogs.Scan(
+				&contactUserLog.EntryID,
+				&contactUserLog.UserID,
+				&contactUserLog.LocationID,
+				&contactUserLog.EntryTime,
+				&contactUserLog.ExitTime,
+			)
+			if err != nil {
+				return entryLogs, err
+			}
+			entryLogs.EntryLogs = append(entryLogs.EntryLogs, contactUserLog)
+		}
 	}
 	return entryLogs, nil
 }
