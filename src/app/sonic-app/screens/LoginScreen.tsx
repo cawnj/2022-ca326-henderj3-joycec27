0d81a1e3a8456cd58532749a2b64a59673c48a28
Jason Henderson <jason.henderson3@mail.dcu.ie>
2022-02-18 04:10:42 +0000

    app/screens: add states and styles to LoginScreen

--- src/app/sonic-app/screens/LoginScreen.tsx
+++ src/app/sonic-app/screens/LoginScreen.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
 
 import {
@@ -9,24 +10,27 @@ import {
 } from "../components/Themed";
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <KeyboardAvoidingView style={styles.container}>
-      <View style={styles.container}>
+      <View style={styles.inputContainer}>
         <TextInput
           placeholder="Email"
-          // value={ }
-          // onChangeText={text => ""}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
           style={styles.input}
         />
         <TextInput
           placeholder="Password"
-          // value={ }
-          // onChangeText={text => ""}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
           style={styles.input}
           secureTextEntry
         />
       </View>
-      <View style={styles.container}>
+      <View style={[styles.container, styles.buttonContainer]}>
         <TouchableOpacity onPress={() => {}} style={styles.button}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
@@ -58,13 +62,41 @@ const styles = StyleSheet.create({
     height: 1,
     width: "80%",
   },
-  input: {},
+  inputContainer: {
+    width: "80%",
+    marginTop: 100,
+  },
+  input: {
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: "60%",
+    marginTop: 40,
+  },
   button: {
+    backgroundColor: "#0782F9",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
   },
   buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
   },
   buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#0782F9",
+    borderWidth: 2,
   },
   buttonOutlineText: {
+    color: "#0782F9",
+    fontWeight: "700",
+    fontSize: 16,
   },
 });
