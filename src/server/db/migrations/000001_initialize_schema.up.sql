dd1c03969fa0e67b44f107998c3c11f216b05865
Conor Joyce <conor.joyce27@mail.dcu.ie>
2022-02-21 21:49:04 +0000

    server/db: update users table schema

--- src/server/db/migrations/000001_initialize_schema.up.sql
+++ src/server/db/migrations/000001_initialize_schema.up.sql
@@ -1,10 +1,8 @@
 -- initialize our schema and desired tables
 
 CREATE TABLE IF NOT EXISTS users(
-  user_id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  phone_number VARCHAR(10) NOT NULL,
-  covid_positive BOOLEAN NOT NULL
+  user_id VARCHAR(100) PRIMARY KEY,
+  expo_token VARCHAR(100) NOT NULL
 );
 CREATE TABLE IF NOT EXISTS locations(
   location_id SERIAL PRIMARY KEY,
@@ -13,7 +11,7 @@ CREATE TABLE IF NOT EXISTS locations(
 );
 CREATE TABLE IF NOT EXISTS entry_log(
   entry_id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users (user_id),
+  user_id VARCHAR(100) REFERENCES users (user_id),
   location_id INTEGER REFERENCES locations (location_id),
   entry_time TIMESTAMP,
   exit_time TIMESTAMP
