feae501ba2ec6ab933b8cf1ce80ed64ac6726aa5
Conor Joyce <conor.joyce27@mail.dcu.ie>
2022-02-21 23:27:31 +0000

    server/db: create RegisterUser function

--- src/server/db/user.go
+++ src/server/db/user.go
@@ -39,6 +39,28 @@ func (db Database) GetAllUsers() (*models.UserList, error) {
 	return users, nil
 }
 
-func (db Database) RegisterUser(user *models.User) error {
-	return nil
+func (db Database) RegisterUser(userReq *models.User) error {
+	user, err := db.GetUser(userReq.UserID)
+	if err != nil && err != ErrNoMatch {
+		return err
+	}
+
+	var query string
+	switch {
+	// user exists, update token
+	case user != nil && userReq.ExpoToken != user.ExpoToken:
+		query = `UPDATE users
+		SET expo_token = $2
+		WHERE user_id = $1`
+	// user does not exist, insert new row
+	case user == nil:
+		query = `INSERT INTO users (user_id, expo_token)
+		VALUES ($1, $2)`
+	// user exists but has not changed, return
+	default:
+		return nil
+	}
+
+	_, err = db.Conn.Exec(query, userReq.UserID, userReq.ExpoToken)
+	return err
 }
